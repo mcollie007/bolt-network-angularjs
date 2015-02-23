1b45a047b66f84ef7be79d1bbdda95f74610cde1
Michael Collie-Acasio <mcollie_fhc@hotmail.com>
2015-02-23 03:06:05 -0500

    Bolt-Network 2 -Leve1 2 -Project 1
    http://s3.amazonaws.com/codecademy-content/projects/4/bolt-network-2/REA
    DME.html
    
    Using directives in angular js

---
+++ js/directives/programListing.js
@@ -0,0 +1,13 @@
+app.directive('programListing', function(){
+    return {
+        restrict: 'E',
+        scope:{
+            listing: '='
+        },
+        templateUrl: 'js/directives/installApp.html'
+    
+    
+    };
+
+
+});
